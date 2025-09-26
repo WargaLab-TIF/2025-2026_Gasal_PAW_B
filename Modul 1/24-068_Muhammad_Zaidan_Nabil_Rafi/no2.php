8916ab65a71c843c346e309d1891e08cfa26f123
zaidan <zaidannabil2212@gmail.com>
2025-09-26 19:25:16 +0700

    Modul 1 sudah selesai

---
+++ Modul 1/24-068_Muhammad_Zaidan_Nabil_Rafi/no2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP dan HTML</title>
+</head>
+<body>
+    <h1>Contoh PHP di HTML</h1>
+    <p>
+        <?php
+        echo "haii nama ku zaidan";
+        ?>
+    </p>
+</body>
+</html>
---
+++ Modul 1/24-068_Muhammad_Zaidan_Nabil_Rafi/no5.php
@@ -0,0 +1,11 @@
+<?php
+$nama = "Zaidan";     
+$umur = 19;           
+$tinggi = 165.5;      
+$isMahasiswa = true;  
+
+echo "Nama: $nama <br>";
+echo "Umur: $umur <br>";
+echo "Tinggi: $tinggi cm<br>";
+echo "Mahasiswa: " . ($isMahasiswa ? "Ya" : "Tidak");
+?>
