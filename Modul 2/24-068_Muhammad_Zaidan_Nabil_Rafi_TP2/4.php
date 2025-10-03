08279a82018bf5a97ea9c50ef010e4d602d4e786
zaidan <zaidannabil2212@gmail.com>
2025-10-03 19:46:55 +0700

    Modul 2 Selesai

---
+++ Modul 2/24-068_Muhammad_Zaidan_Nabil_Rafi_TP2/4.php
@@ -0,0 +1,17 @@
+<?php
+$menu = "soto";
+
+switch ($menu) {
+    case "nasi goreng":
+        echo "Kamu memilih Nasi Goreng";
+        break;
+    case "bakso":
+        echo "Kamu memilih Bakso";
+        break;
+    case "soto":
+        echo "Kamu memilih Soto";
+        break;
+    default:
+        echo "Menu tidak tersedia";
+}
+?>
