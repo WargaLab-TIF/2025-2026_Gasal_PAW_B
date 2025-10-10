c51f29c098e9442cb124969bd202a231fecbaf26
zaidan <zaidannabil2212@gmail.com>
2025-10-10 18:38:06 +0700

    modul 3 telah selesai

--- Modul 3/24-068_Muhammad_Zaidan_Nabil_Rafi_TP3/2.php
+++ Modul 3/24-068_Muhammad_Zaidan_Nabil_Rafi_TP3/2.php
@@ -9,6 +9,6 @@ for($x = 0; $x < $arrlength; $x++) {
 echo "Jumlah data: $arrlength <br>";
 $vegies = array("Carrot", "Broccoli", "Spinach");
 for($x = 0; $x < count($vegies); $x++) {
-    echo $vegies[$x] . "<br>";
+    echo $vegies[$x] . "<br>"; 
 }
 ?>
