50d0ad3c097b1f8a4f189421d70ca358e65f0f52
4uriel <achoirilanwar01@gmail.com>
2025-10-11 18:24:33 +0700

    UPDATE NO 1

--- Modul 3/24-098_A.Choiril_Anwar_EL.A.R/tugas-1.php
+++ Modul 3/24-098_A.Choiril_Anwar_EL.A.R/tugas-1.php
@@ -3,17 +3,25 @@
 $fruits = array("Avocado", "Blueberry", "Cherry");
 echo "I like " . $fruits[0] . ", " . $fruits[1] . " and " . $fruits[2] . ".";
 
+echo "<br>";
 //nambah 5 buah
 array_push($fruits, "Durian", "Elderberry","Apel","Mangga","Jeruk");
-//nampilin index tertinggi
-$nilai_index=$fruits[$index-1];
-echo "$nilai_index";
+echo "array yg sudah di tambah: ";
+foreach($fruits as $buah){
+    echo "$buah , ";
+}
+
 //nampilin berapa index
 $index = count($fruits);
 echo "index : $index <br>";
 
+//nampilin index tertinggi
+$nilai_index=$fruits[$index-1];
+echo "$nilai_index <br>";
+
+
 // hapus 1 data
-unset($fruits[7]);
+unset($fruits[7]); 
 $index = count($fruits);
 echo "index : $index <br>";
 $nilai_index=$fruits[$index-1];
