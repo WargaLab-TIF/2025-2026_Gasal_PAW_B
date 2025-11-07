3153fac2b7baed56d18cc5e2f65458e7cf9c0823
Sigiitttt <sigitaringga2@gmail.com>
2025-11-07 20:34:22 +0700

    tugas 6 done

---
+++ Modul 6/23-104_Moch_Sigit_Aringga/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Password123!";
+$dbname = "tp6newpol";
+
+
+try {
+    // Buat koneksi PDO
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi database gagal: " . $e->getMessage());
+}
+?>
